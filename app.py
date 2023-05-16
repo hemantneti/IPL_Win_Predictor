b9d5861fb5bc7f92df25f275b4fe4b9b666d4318
Hemant Neti <84624478+hemantneti@users.noreply.github.com>
2023-05-17 00:16:37 +0530

    app.py

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ teams = ['Sunrisers Hyderabad',
  'Mumbai Indians',
  'Royal Challengers Bangalore',
  'Kolkata Knight Riders',
- 'Kings XI Punjab',
+ 'Punjab Kings',
  'Chennai Super Kings',
  'Rajasthan Royals',
- 'Delhi Capitals']
+ 'Delhi Capitals' ,
+ 'Lucknow Super Giant',
+ 'Gujrat Titans']
 
 cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
